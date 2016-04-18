a42d54397a9d9591e49f7b8cee7a0d1afbd7e91a
Alexanderaldy <aldyAlexandermh@gmail.com>
2016-04-18 13:56:59 +0700

    Added files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<?php
+    echo "My first PHP script!";
+?>
+<table style="width:100%">
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td> 
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td> 
+    <td>94</td>
+  </tr>
+</table>
+
+<input type="submit" id="add" onclick="add()">Click Me!</input>
+
+
+</body>
+</html>
