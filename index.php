c3ca39d2470054e4446236df40dd199947b7a22b
Alexanderaldy <aldyAlexandermh@gmail.com>
2016-04-18 14:20:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,20 +7,15 @@
     echo "My first PHP script!";
 ?>
 <table style="width:100%">
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td> 
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td> 
-    <td>94</td>
-  </tr>
+    <tr>
+        <td>Rayon</td>
+        <td>Outlet</td>
+    </tr>
+    <tr>
+        <td>A <input type="submit" id="add" onclick="add()">Add</input></td>
+        <td> <input type="submit" id="add" onclick="add() value="hidden">Delete</input></td>
+    </tr>
 </table>
-
-<input type="submit" id="add" onclick="add()">Click Me!</input>
-
 
 </body>
 </html>
